ec9256c5755a5da6962269e6c5f20755bcd9f1df
Seono-Na <seono5670@gmail.com>
2025-04-09 03:51:10 +0900

    chore: tailwindcss 4 설정 및 vite 플러그인 적용
    - tailwindcss 및 @tailwindcss/vite 패키지 설치
    - vite 설정에 tailwindcss 플러그인 추가
    - index.css에서 tailwindcss 적용을 위한 @import 구문 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()], //tsconfig.json의 경로 alias 사용
+  plugins: [react(), tailwindcss(), tsconfigPaths()], //tsconfig.json의 경로 alias 사용
 });
